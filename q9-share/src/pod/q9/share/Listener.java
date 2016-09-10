adb10ca07d192795c6eb6b62cf8b95528ac7dac1
Ari Garcia <aristofanio@hotmail.com>
2016-09-10 11:05:32 -0300

    adicionado RemoteException na assinatura do método uma vez que ele estende Remote.

--- q9-share/src/pod/q9/share/Listener.java
+++ q9-share/src/pod/q9/share/Listener.java
@@ -1,7 +1,8 @@
 package pod.q9.share;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface Listener extends Remote {
-	void notifyNode1(String text);
+	void notifyNode1(String text) throws RemoteException;
 }
